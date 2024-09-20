3073149d2316a9a9d118301057509f4991fab432
DarkStar <darkstar@unisocial.net>
2024-09-20 13:50:05 -0400

    src

---
+++ src/ansible/dev.sh
@@ -0,0 +1,4 @@
+cd roles
+git clone git@github.com:Metal-AI/ansible-role-MAAS-config.git
+git clone git@github.com:Metal-AI/ansible-role-MAAS-init.git
+git clone git@github.com:Metal-AI/ansible-role-MAAS-install.git
